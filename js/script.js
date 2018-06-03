680e6d13e998ed44b23c7162d8d3a60b3d46526a
zoeyzou <zoeyzou2010@hotmail.com>
2018-06-03 23:03:28 +0200

    MVP -- My first agile approach done!

--- js/script.js
+++ js/script.js
@@ -9,3 +9,38 @@ for(let i = 0; i < navMenuItem.length; i++) {
     this.className += " active";
   });
 }
+
+//to collapse experience entries
+var experienceTitle = document.querySelectorAll(".experience-title");
+var experienceContent = document.querySelectorAll(".experience-content");
+
+for(let i = 0; i < experienceTitle.length; i++) {
+  experienceTitle[i].addEventListener("click", function() {
+    if (experienceContent[i].style.display === "none") {
+      experienceContent[i].style.display = "block";
+    }
+    else {
+      experienceContent[i].style.display = "none";
+    }
+  })
+}
+
+//to loop through work and outside work tabs
+var lifeButton = document.getElementById("life-buttons").children;
+var work = document.getElementById("work");
+var outsideWork = document.getElementById("outside-work");
+
+
+
+for (let i = 0; i < lifeButton.length; i++) {
+  lifeButton[i].addEventListener("click", function() {
+    if (i === 0) {
+      work.style.display = "block";
+      outsideWork.style.display = "none";
+    }
+    else {
+      work.style.display = "none";
+      outsideWork.style.display = "block";
+    }
+  });
+}
